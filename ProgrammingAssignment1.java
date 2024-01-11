4134809c0078c622661a31380185e1002f9aafec
ChrisR6455 <156102013+ChrisR6455@users.noreply.github.com>
2024-01-11 10:13:03 -0500

    Add files via upload

---
+++ ProgrammingAssignment1.java
@@ -0,0 +1,15 @@
+public class ProgrammingAssignment1
+{
+   // main: generate some simple output
+   
+   public static void main(String[] args)
+   {
+        System.out.println("********    ********");
+        System.out.println("*           *     **");
+        System.out.println("*           *    ** ");
+        System.out.println("*           * **  ");
+        System.out.println("*           *   *   ");
+        System.out.println("*           *   **    ");
+        System.out.println("********    *     **   ");
+   }
+}
